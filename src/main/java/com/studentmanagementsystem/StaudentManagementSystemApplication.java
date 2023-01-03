be96f690b163c139d2a4be921afc792eeced612a
Satyam Suman <findsatyam95@gmail.com>
2023-01-03 21:40:27 +0530

    Initial Commit

---
+++ src/main/java/com/studentmanagementsystem/StaudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.studentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaudentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaudentManagementSystemApplication.class, args);
+    }
+
+}
