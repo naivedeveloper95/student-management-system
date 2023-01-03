3ca180e6b7f5b1d5b361cefbc0c4c190662ce91a
Satyam Suman <findsatyam95@gmail.com>
2023-01-03 23:55:28 +0530

    Added view for Add and List Student

--- src/main/java/com/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/studentmanagementsystem/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.studentmanagementsystem.controller;
 
+import com.studentmanagementsystem.entity.Student;
 import com.studentmanagementsystem.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -18,5 +21,20 @@ public class StudentController {
     public String listStudents(Model model) {
         model.addAttribute("students", studentService.getAllStudent());
         return "students";
+    }
+
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model) {
+
+        Student student = new Student();
+        model.addAttribute("student", student);
+
+        return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") Student student) {
+        studentService.saveStudent(student);
+        return "redirect:/students";
     }
 }
--- src/main/java/com/studentmanagementsystem/service/StudentService.java
+++ src/main/java/com/studentmanagementsystem/service/StudentService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface StudentService {
     List<Student> getAllStudent();
+    Student saveStudent(Student student);
 }
